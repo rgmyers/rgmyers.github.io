d3fbc594cb70e1003087b19cca9813371c7040e2
rgmyers <rgmyers1@gmail.com>
2021-02-25 21:15:47 -0400

    check temp

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const town = "Charlottetown";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=${key}&units=metric`
 
 const weather = new weather(town);
+let tempElem = document.querySelector(".temp");
+tempElem.innerHTML += weather.temp;
