ade14ba50bcfe7e2e86445fbc7b2876045db8210
rgmyers <rgmyers1@gmail.com>
2021-02-25 21:25:26 -0400

    updated class

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+
 const key = "3a8f879f9171d9be0e7e8e7867670af7";
 const town = "Charlottetown";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=${key}&units=metric`
 
-const weather = new weather(town);
+let weatherTest = new weather(town);
 let tempElem = document.querySelector(".temp");
 tempElem.innerHTML += weather.temp;
