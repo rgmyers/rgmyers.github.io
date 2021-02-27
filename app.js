f6254392ff6b7f0e0cb2016858a872b7bb51ada9
rgmyers <rgmyers1@gmail.com>
2021-02-26 22:59:56 -0400

    added city

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const key = "3a8f879f9171d9be0e7e8e7867670af7";
 //When the page loads run this function.
 window.addEventListener('load', ()=>{
-    let long;
+    let lon;
     let lat;
     let temperatureDegree = document.querySelector(".temperature-degree");
     let temperatureDescription = document.querySelector(".temperature-description");
     let locationTimezone = document.querySelector(".location-timezone");
+    let locationCity = document.querySelector(".location-city");
     const Kelvin = 273;
     //Checks to see if they allow us to know their location if so take variables and populate them with location.
     if(navigator.geolocation){
@@ -23,12 +24,18 @@ window.addEventListener('load', ()=>{
                 console.log(data);
                 const temperature = (data.main.temp)-Kelvin;
                 const timezone = data.timezone;
-                const summary = data.weather[0].description;        
+                const summary = data.weather[0].description;
+                const test = data.name;
+                console.log(`test results: ${test}`);        
                 console.log(temperature);
                 console.log(timezone);
                 console.log(summary);
-                temperatureDegree.innerHTML = Math.floor(temperature); 
+                
+                temperatureDegree.textContent = Math.floor(temperature); 
                 temperatureDescription.textContent = summary;
+                locationTimezone.textContent = timezone;
+                locationCity.textContent = data.name;
+                
                 console.log(`This is tempDescription ${temperatureDescription}`);      
             });
             
